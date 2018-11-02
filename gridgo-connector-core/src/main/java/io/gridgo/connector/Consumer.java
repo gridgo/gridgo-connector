89ad6ff2984639e27ec15b26425906d1f048f8e9
anbui <anbui@ANBUI-IN.mshome.net>
2018-11-02 23:11:44 +0800

    add fluent interface

--- gridgo-connector-core/src/main/java/io/gridgo/connector/Consumer.java
+++ gridgo-connector-core/src/main/java/io/gridgo/connector/Consumer.java
@@ -7,9 +7,9 @@ import io.gridgo.framework.execution.ExecutionStrategy;
 
 public interface Consumer {
 
-	public void subscribe(java.util.function.BiConsumer<Message, Deferred<Message, Throwable>> subscriber);
+	public Consumer subscribe(java.util.function.BiConsumer<Message, Deferred<Message, Throwable>> subscriber);
 
-	public void subscribe(java.util.function.Consumer<Message> subscriber);
+	public Consumer subscribe(java.util.function.Consumer<Message> subscriber);
 
-	public void invokeCallbackOn(ExecutionStrategy strategy);
+	public Consumer invokeCallbackOn(ExecutionStrategy strategy);
 }
