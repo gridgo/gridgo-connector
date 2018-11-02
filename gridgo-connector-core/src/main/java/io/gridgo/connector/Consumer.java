fa933999b3ad5ffdb02c4e80aa4d0f741e9e144b
anbui <anbui@ANBUI-IN.mshome.net>
2018-11-02 23:02:18 +0800

    integrate execution strategy

--- gridgo-connector-core/src/main/java/io/gridgo/connector/Consumer.java
+++ gridgo-connector-core/src/main/java/io/gridgo/connector/Consumer.java
@@ -1,8 +1,11 @@
 package io.gridgo.connector;
 
 import io.gridgo.connector.message.Message;
+import io.gridgo.framework.execution.ExecutionStrategy;
 
 public interface Consumer {
 
 	public void subscribe(java.util.function.Consumer<Message> subscriber);
+	
+	public void invokeCallbackOn(ExecutionStrategy strategy);
 }
--- gridgo-connector-core/src/main/java/io/gridgo/connector/Producer.java
+++ gridgo-connector-core/src/main/java/io/gridgo/connector/Producer.java
@@ -3,6 +3,7 @@ package io.gridgo.connector;
 import org.joo.promise4j.Promise;
 
 import io.gridgo.connector.message.Message;
+import io.gridgo.framework.execution.ExecutionStrategy;
 
 public interface Producer {
 
@@ -11,4 +12,8 @@ public interface Producer {
 	public Promise<Message, Throwable> send(Message message, boolean sendWithAck);
 
 	public Promise<Message, Throwable> call(Message request);
+	
+	public void produceOn(ExecutionStrategy strategy);
+	
+	public void invokeCallbackOn(ExecutionStrategy strategy);
 }
