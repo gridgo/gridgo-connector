08482617a1ad8014f022414796e6384f680012c5
anbui <anbui@ANBUI-IN.mshome.net>
2018-11-03 10:33:10 +0800

    support connector config

---
+++ gridgo-connector-core/src/main/java/io/gridgo/connector/impl/resolvers/DefaultConnectorConfig.java
@@ -0,0 +1,19 @@
+package io.gridgo.connector.impl.resolvers;
+
+import java.util.Map;
+
+import io.gridgo.connector.ConnectorConfig;
+import lombok.Getter;
+
+@Getter
+public class DefaultConnectorConfig implements ConnectorConfig {
+	
+	private String remaining;
+
+	private Map<String, Object> parameters;
+
+	public DefaultConnectorConfig(String remaining, Map<String, Object> parameters) {
+		this.remaining = remaining;
+		this.parameters = parameters;
+	}
+}
