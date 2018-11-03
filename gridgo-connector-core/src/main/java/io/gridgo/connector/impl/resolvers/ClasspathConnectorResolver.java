9cc9b30570fdda7fec75c5d2c6dfb2a7c1b0f024
anbui <anbui@ANBUI-IN.mshome.net>
2018-11-03 10:33:53 +0800

    add classpath & uri connection resolver

---
+++ gridgo-connector-core/src/main/java/io/gridgo/connector/impl/resolvers/ClasspathConnectorResolver.java
@@ -0,0 +1,77 @@
+package io.gridgo.connector.impl.resolvers;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import org.reflections.Reflections;
+
+import io.gridgo.connector.Connector;
+import io.gridgo.connector.ConnectorResolver;
+import io.gridgo.connector.annotations.ConnectorEndpoint;
+import io.gridgo.connector.support.exceptions.UnsupportedSchemeException;
+import lombok.NonNull;
+
+public class ClasspathConnectorResolver implements ConnectorResolver {
+
+	private final static String DEFAULT_PACKAGE = "io.gridgo.connector";
+
+	private Map<String, Class<? extends Connector>> classMappings = new HashMap<>();
+
+	public ClasspathConnectorResolver() {
+		this(DEFAULT_PACKAGE);
+	}
+
+	public ClasspathConnectorResolver(final @NonNull String... packages) {
+		resolveClasspaths(packages);
+	}
+
+	private void resolveClasspaths(String[] packages) {
+		for (String pkg : packages) {
+			resolvePackage(pkg);
+		}
+	}
+
+	private void resolvePackage(String pkg) {
+		Reflections reflections = new Reflections(pkg);
+		Set<Class<? extends Connector>> connectorClasses = reflections.getSubTypesOf(Connector.class);
+		
+		if (connectorClasses.isEmpty()) {
+			// TODO log warning
+			return;
+		}
+		
+		for (Class<? extends Connector> clzz : connectorClasses) {
+			registerConnectorClass(clzz);
+		}
+	}
+
+	private void registerConnectorClass(Class<? extends Connector> clzz) {
+		ConnectorEndpoint[] endpointAnnotations = clzz.getAnnotationsByType(ConnectorEndpoint.class);
+		if (endpointAnnotations.length != 1) {
+			// TODO log warning
+			return;
+		}
+		ConnectorEndpoint endpoint = endpointAnnotations[0];
+		String scheme = endpoint.scheme();
+		if (classMappings.containsKey(scheme)) {
+			// TODO log warning
+		}
+		classMappings.putIfAbsent(scheme, clzz);
+	}
+
+	@Override
+	public Connector resolve(final @NonNull String endpoint) {
+		String scheme = endpoint, remaining = "";
+		int schemeIdx = endpoint.indexOf(':');
+		if (schemeIdx != -1) {
+			scheme = endpoint.substring(0, schemeIdx);
+			remaining = endpoint.substring(schemeIdx + 1);
+		}
+
+		Class<? extends Connector> clzz = classMappings.get(scheme);
+		if (clzz == null)
+			throw new UnsupportedSchemeException(scheme);
+		return new UriConnectorResolver(clzz).resolve(remaining);
+	}
+}
---
+++ gridgo-connector-core/src/main/java/io/gridgo/connector/impl/resolvers/UriConnectorResolver.java
@@ -0,0 +1,57 @@
+package io.gridgo.connector.impl.resolvers;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.Map;
+
+import io.gridgo.connector.Connector;
+import io.gridgo.connector.ConnectorConfig;
+import io.gridgo.connector.ConnectorResolver;
+import io.gridgo.connector.support.exceptions.ConnectorResolutionException;
+
+public class UriConnectorResolver implements ConnectorResolver {
+
+	private final Class<? extends Connector> clazz;
+
+	public UriConnectorResolver(Class<? extends Connector> clazz) {
+		this.clazz = clazz;
+	}
+
+	@Override
+	public Connector resolve(String endpoint) {
+		try {
+			ConnectorConfig config = resolveProperties(endpoint);
+			return clazz.getConstructor(ConnectorConfig.class).newInstance(config);
+		} catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException
+				| NoSuchMethodException | SecurityException e) {
+			throw new ConnectorResolutionException("Exception caught while resolving endpoint " + endpoint, e);
+		}
+	}
+
+	private ConnectorConfig resolveProperties(String endpoint) {
+		String schemePart = endpoint;
+		String queryPart = null;
+
+		int queryPartIdx = endpoint.indexOf('?');
+		if (queryPartIdx != -1) {
+			queryPart = endpoint.substring(queryPartIdx + 1);
+			schemePart = endpoint.substring(0, queryPartIdx);
+		}
+
+		Map<String, Object> params = extractParameters(queryPart);
+		return new DefaultConnectorConfig(schemePart, params);
+	}
+
+	private Map<String, Object> extractParameters(String queryPath) {
+		Map<String, Object> params = new HashMap<>();
+
+		String[] queries = queryPath.split("&");
+		for (String query : queries) {
+			String[] keyValuePair = query.split("=");
+			if (keyValuePair.length == 2)
+				params.put(keyValuePair[0], keyValuePair[1]);
+		}
+
+		return params;
+	}
+}
