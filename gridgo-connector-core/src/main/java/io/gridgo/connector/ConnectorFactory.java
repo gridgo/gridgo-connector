340fc988e091a75b7af2232ebcdffe8f3763f322
anbui <anbui@ANBUI-IN.mshome.net>
2018-11-03 10:32:53 +0800

    clean methods

--- gridgo-connector-core/src/main/java/io/gridgo/connector/ConnectorFactory.java
+++ gridgo-connector-core/src/main/java/io/gridgo/connector/ConnectorFactory.java
@@ -4,17 +4,7 @@ import lombok.NonNull;
 
 public interface ConnectorFactory {
 
-	public default Connector createConnector(final @NonNull String endpoint) {
-		return createConnector(endpoint, false);
-	}
+	public Connector createConnector(final @NonNull String endpoint);
 	
-	public default Connector createConnector(final @NonNull String endpoint, final @NonNull ConnectorResolver resolver) {
-		return createConnector(endpoint, resolver, false);
-	}
-
-	public Connector createConnector(final @NonNull String endpoint, boolean prototype);
-
-	public Connector createConnector(final @NonNull String endpoint, final @NonNull ConnectorResolver resolver, boolean prototype);
-	
-	public ConnectorFactory setDefaultConnectorResolver(final @NonNull ConnectorResolver resolver);
+	public Connector createConnector(final @NonNull String endpoint, final @NonNull ConnectorResolver resolver);
 }
