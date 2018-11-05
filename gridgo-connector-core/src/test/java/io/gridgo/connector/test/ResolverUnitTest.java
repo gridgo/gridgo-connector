5fcf2b89ad770ec26ffc4bc1f0d29f827edd83bb
anbui <anbui@ANBUI-IN.mshome.net>
2018-11-05 15:51:15 +0800

    split unit tests

--- gridgo-connector-core/src/test/java/io/gridgo/connector/test/ResolverUnitTest.java
+++ gridgo-connector-core/src/test/java/io/gridgo/connector/test/ResolverUnitTest.java
@@ -25,19 +25,25 @@ public class ResolverUnitTest {
 		Assert.assertEquals("tcp", connector.getConnectorConfig().getPlaceholders().get("transport"));
 		Assert.assertEquals("127.0.0.1", connector.getConnectorConfig().getPlaceholders().get("host"));
 		Assert.assertEquals("8080", connector.getConnectorConfig().getPlaceholders().get("port"));
-		
-		connector = resolver.resolve("test:pull:tcp://[2001:db8:1f70::999:de8:7648:6e8]:8080?p1=v1&p2=v2");
+	}
+
+	@Test
+	public void testIPv6() {
+		ConnectorResolver resolver = new ClasspathConnectorResolver();
+		Connector connector = resolver.resolve("test:pull:tcp://[2001:db8:1f70::999:de8:7648:6e8]:8080?p1=v1&p2=v2");
 		Assert.assertNotNull(connector);
 		Assert.assertNotNull(connector.getConnectorConfig());
 		Assert.assertNotNull(connector.getConnectorConfig().getRemaining());
 		Assert.assertNotNull(connector.getConnectorConfig().getParameters());
 		Assert.assertTrue(connector instanceof TestConnector);
-		Assert.assertEquals("pull:tcp://[2001:db8:1f70::999:de8:7648:6e8]:8080", connector.getConnectorConfig().getRemaining());
+		Assert.assertEquals("pull:tcp://[2001:db8:1f70::999:de8:7648:6e8]:8080",
+				connector.getConnectorConfig().getRemaining());
 		Assert.assertEquals("v1", connector.getConnectorConfig().getParameters().get("p1"));
 		Assert.assertEquals("v2", connector.getConnectorConfig().getParameters().get("p2"));
 		Assert.assertEquals("pull", connector.getConnectorConfig().getPlaceholders().get("type"));
 		Assert.assertEquals("tcp", connector.getConnectorConfig().getPlaceholders().get("transport"));
-		Assert.assertEquals("[2001:db8:1f70::999:de8:7648:6e8]", connector.getConnectorConfig().getPlaceholders().get("host"));
+		Assert.assertEquals("[2001:db8:1f70::999:de8:7648:6e8]",
+				connector.getConnectorConfig().getPlaceholders().get("host"));
 		Assert.assertEquals("8080", connector.getConnectorConfig().getPlaceholders().get("port"));
 	}
 }
