6fd1e3336cb21d4a079903bc393cd1bf1e11e47f
anbui <anbui@ANBUI-IN.mshome.net>
2018-11-07 00:28:50 +0800

    add default method

--- core/src/main/java/io/gridgo/connector/Consumer.java
+++ core/src/main/java/io/gridgo/connector/Consumer.java
@@ -9,7 +9,10 @@ import lombok.NonNull;
 
 public interface Consumer extends ComponentLifecycle, CallbackExecutionAware<Consumer> {
 
-	public Consumer subscribe(final @NonNull java.util.function.BiConsumer<Message, Deferred<Message, Exception>> subscriber);
+	public Consumer subscribe(
+			final @NonNull java.util.function.BiConsumer<Message, Deferred<Message, Exception>> subscriber);
 
-	public Consumer subscribe(final @NonNull java.util.function.Consumer<Message> subscriber);
+	public default Consumer subscribe(final @NonNull java.util.function.Consumer<Message> subscriber) {
+		return subscribe((msg, deferred) -> subscriber.accept(msg));
+	}
 }
