63739459238ac1aa07c8dfe29907ff4f690e5fa0
anbui <anbui@ANBUI-IN.mshome.net>
2018-11-07 15:47:58 +0800

    add callback for response

--- core/src/main/java/io/gridgo/connector/impl/AbstractProducer.java
+++ core/src/main/java/io/gridgo/connector/impl/AbstractProducer.java
@@ -6,35 +6,36 @@ import io.gridgo.connector.Producer;
 import io.gridgo.connector.support.exceptions.SendMessageException;
 import io.gridgo.framework.AbstractComponentLifecycle;
 import io.gridgo.framework.execution.ExecutionStrategy;
+import io.gridgo.framework.execution.impl.DefaultExecutionStrategy;
 import io.gridgo.framework.support.Message;
 import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NonNull;
 
 public abstract class AbstractProducer extends AbstractComponentLifecycle implements Producer {
 
+	private static final ExecutionStrategy DEFAULT_CALLBACK_EXECUTOR = new DefaultExecutionStrategy();
+
 	@Getter(AccessLevel.PROTECTED)
-	private ExecutionStrategy callbackInvokeExecutor;
+	private ExecutionStrategy callbackInvokeExecutor = DEFAULT_CALLBACK_EXECUTOR;
 
 	@Override
-	public Producer invokeCallbackOn(ExecutionStrategy strategy) {
+	public Producer invokeCallbackOn(final @NonNull ExecutionStrategy strategy) {
 		this.callbackInvokeExecutor = strategy;
 		return this;
 	}
 
 	protected void callback(Deferred<Message, Exception> deferred, Exception exception) {
-		boolean success = exception == null;
-		Runnable deferredFinisher = () -> {
-			if (success) {
+		callbackInvokeExecutor.execute(() -> {
+			if (exception == null) {
 				deferred.resolve(null);
 			} else {
 				deferred.reject(exception != null ? exception : new SendMessageException(exception));
 			}
-		};
+		});
+	}
 
-		if (this.getCallbackInvokeExecutor() != null) {
-			this.getCallbackInvokeExecutor().execute(deferredFinisher);
-		} else {
-			deferredFinisher.run();
-		}
+	protected void callback(Deferred<Message, Exception> deferred, Message response) {
+		callbackInvokeExecutor.execute(() -> deferred.resolve(response));
 	}
 }
