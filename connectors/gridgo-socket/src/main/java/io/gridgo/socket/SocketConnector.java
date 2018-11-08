0d01560fb4b086f40bd66dbfacb1b3f0b9e7edb2
anbui <anbui@ANBUI-IN.mshome.net>
2018-11-08 13:04:54 +0800

    implement onStart/onStop

--- connectors/gridgo-socket/src/main/java/io/gridgo/socket/SocketConnector.java
+++ connectors/gridgo-socket/src/main/java/io/gridgo/socket/SocketConnector.java
@@ -7,6 +7,7 @@ import io.gridgo.connector.Connector;
 import io.gridgo.connector.Consumer;
 import io.gridgo.connector.Producer;
 import io.gridgo.connector.support.config.ConnectorConfig;
+import io.gridgo.framework.AbstractComponentLifecycle;
 import lombok.Getter;
 
 /**
@@ -18,16 +19,20 @@ import lombok.Getter;
  * @author bachden
  *
  */
-public class SocketConnector implements Connector {
+public class SocketConnector extends AbstractComponentLifecycle implements Connector {
 
 	private final SocketFactory factory;
 
 	private String type;
 	private String address;
 	private Map<String, Object> params;
+	private Optional<Producer> producer = Optional.empty();
+	private Optional<Consumer> consumer = Optional.empty();
 
 	@Getter
 	private ConnectorConfig connectorConfig;
+
+	private Socket socket;
 
 	protected SocketConnector(SocketFactory factory) {
 		this.factory = factory;
@@ -44,22 +49,45 @@ public class SocketConnector implements Connector {
 		this.address = transport + "://" + host + ":" + port;
 		this.params = config.getParameters();
 		this.type = type;
-
+		
 		return this;
 	}
+	
+	@Override
+	protected void onStart() {
+		this.consumer = createConsumer();
+		this.producer = createProducer();
+		
+		if (this.consumer.isPresent())
+			this.consumer.get().start();
+		if (this.producer.isPresent())
+			this.producer.get().start();
+	}
 
-	private Socket initSocket() {
-		SocketOptions options = new SocketOptions();
-		options.setType(type);
-		options.getConfig().putAll(this.params);
-		Socket socket = factory.createSocket(options);
-		return socket;
+	@Override
+	protected void onStop() {
+		if (this.consumer.isPresent())
+			this.consumer.get().stop();
+		if (this.producer.isPresent())
+			this.producer.get().stop();
+		
+		if (this.socket != null)
+			this.socket.close();
 	}
 
 	@Override
 	public Optional<Producer> getProducer() {
+		return producer;
+	}
+
+	@Override
+	public Optional<Consumer> getConsumer() {
+		return consumer;
+	}
+	
+	private Optional<Producer> createProducer() {
 		if (type.equalsIgnoreCase("push") || type.equalsIgnoreCase("pub")) {
-			Socket socket = initSocket();
+			this.socket = initSocket();
 			switch (type) {
 			case "push":
 				socket.connect(address);
@@ -70,13 +98,12 @@ public class SocketConnector implements Connector {
 			}
 			return Optional.of(SocketProducer.newDefault(socket));
 		}
-		return Optional.ofNullable(null);
+		return Optional.empty();
 	}
 
-	@Override
-	public Optional<Consumer> getConsumer() {
+	private Optional<Consumer> createConsumer() {
 		if (type.equalsIgnoreCase("pull") || type.equalsIgnoreCase("sub")) {
-			Socket socket = initSocket();
+			this.socket = initSocket();
 			switch (type) {
 			case "pull":
 				socket.bind(this.address);
@@ -87,6 +114,14 @@ public class SocketConnector implements Connector {
 			}
 			return Optional.of(SocketConsumer.newDefault(socket));
 		}
-		return Optional.ofNullable(null);
+		return Optional.empty();
+	}
+
+	private Socket initSocket() {
+		SocketOptions options = new SocketOptions();
+		options.setType(type);
+		options.getConfig().putAll(this.params);
+		Socket socket = factory.createSocket(options);
+		return socket;
 	}
 }
