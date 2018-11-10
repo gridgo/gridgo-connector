281ac6c7351f8cd3377b55956e6ee8e427140903
anbui <anbui@ANBUI-IN.mshome.net>
2018-11-10 13:48:04 +0800

    add unit tests

--- core/src/main/java/io/gridgo/connector/support/ConnectionRef.java
+++ core/src/main/java/io/gridgo/connector/support/ConnectionRef.java
@@ -6,24 +6,34 @@ import lombok.Getter;
 
 @Getter
 public class ConnectionRef<T> {
-	
+
 	private T connection;
-	
+
 	private AtomicInteger refCount = new AtomicInteger(0);
 
 	public ConnectionRef(T connection) {
 		this.connection = connection;
 	}
-	
+
 	public int getRefCount() {
 		return refCount.get();
 	}
-	
+
 	public int ref() {
 		return refCount.incrementAndGet();
 	}
-	
+
 	public int deref() {
-		return refCount.decrementAndGet();
+		int value = -1;
+		int ref = 0;
+		do {
+			value = refCount.get();
+			ref = value > 0 ? value - 1 : 0;
+		} while (!refCount.compareAndSet(value, ref));
+		return ref;
+	}
+
+	public void dispose() {
+		this.connection = null;
 	}
 }
