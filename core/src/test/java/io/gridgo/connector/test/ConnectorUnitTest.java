ea65428e9269438a93490ceede89b3e988181e62
anbui <anbui@ANBUI-IN.mshome.net>
2018-11-10 14:14:56 +0800

    add unit tests

---
+++ core/src/test/java/io/gridgo/connector/test/ConnectorUnitTest.java
@@ -0,0 +1,65 @@
+package io.gridgo.connector.test;
+
+import java.util.concurrent.CountDownLatch;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import io.gridgo.bean.BValue;
+import io.gridgo.connector.impl.factories.DefaultConnectorFactory;
+import io.gridgo.connector.test.support.TestConsumer;
+import io.gridgo.connector.test.support.TestProducer;
+import io.gridgo.framework.support.Message;
+import io.gridgo.framework.support.Payload;
+
+public class ConnectorUnitTest {
+
+	@Test
+	public void testConsumer() {
+		var connector = new DefaultConnectorFactory().createConnector("test:pull:tcp://127.0.0.1:8080?p1=v1&p2=v2");
+		var consumer = (TestConsumer) connector.getConsumer().orElseThrow();
+		connector.start();
+		var latch = new CountDownLatch(1);
+		consumer.subscribe(msg -> {
+			if (msg.getPayload().getHeaders().getInteger("test-header") == 1)
+				latch.countDown();
+		});
+		consumer.testPublish();
+		try {
+			latch.await();
+		} catch (InterruptedException e) {
+
+		}
+		connector.stop();
+	}
+
+	@Test
+	public void testProducer() {
+		var connector = new DefaultConnectorFactory().createConnector("test:pull:tcp://127.0.0.1:8080?p1=v1&p2=v2");
+		var producer = (TestProducer) connector.getProducer().orElseThrow();
+		connector.start();
+
+		producer.send(null);
+
+		var sendLatch = new CountDownLatch(1);
+		producer.sendWithAck(null).done(ack -> sendLatch.countDown());
+		try {
+			sendLatch.await();
+		} catch (InterruptedException e) {
+			Assert.fail(e.getMessage());
+		}
+
+		var callLatch = new CountDownLatch(1);
+		producer.call(Message.newDefault(Payload.newDefault(BValue.newDefault(1)))).done(response -> {
+			if (response.getPayload().getBody().asValue().getInteger() == 2)
+				callLatch.countDown();
+		});
+		try {
+			callLatch.await();
+		} catch (InterruptedException e) {
+			Assert.fail(e.getMessage());
+		}
+
+		connector.stop();
+	}
+}
--- core/src/test/java/io/gridgo/connector/test/support/TestProducer.java
+++ core/src/test/java/io/gridgo/connector/test/support/TestProducer.java
@@ -1,40 +1,39 @@
 package io.gridgo.connector.test.support;
 
 import org.joo.promise4j.Promise;
+import org.joo.promise4j.impl.SimpleDonePromise;
 
+import io.gridgo.bean.BValue;
 import io.gridgo.connector.impl.AbstractProducer;
 import io.gridgo.framework.support.Message;
+import io.gridgo.framework.support.Payload;
 
 public class TestProducer extends AbstractProducer {
 
 	@Override
 	public void send(Message message) {
-		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public Promise<Message, Exception> sendWithAck(Message message) {
-		// TODO Auto-generated method stub
-		return null;
+		return new SimpleDonePromise<>(null);
 	}
 
 	@Override
 	public Promise<Message, Exception> call(Message request) {
-		// TODO Auto-generated method stub
-		return null;
+		int body = request.getPayload().getBody().asValue().getInteger();
+		return new SimpleDonePromise<>(Message.newDefault(Payload.newDefault(BValue.newDefault(body + 1))));
 	}
 
 	@Override
 	protected void onStart() {
-		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	protected void onStop() {
-		// TODO Auto-generated method stub
-		
+
 	}
 
 }
