58b6c4e8a29f698a4553a6eddd65af4f041a455e
anbui <anbui@ANBUI-IN.mshome.net>
2018-11-10 17:33:26 +0800

    add tests

--- core/src/test/java/io/gridgo/connector/test/support/TestConnector.java
+++ core/src/test/java/io/gridgo/connector/test/support/TestConnector.java
@@ -8,8 +8,25 @@ import io.gridgo.connector.support.annotations.ConnectorEndpoint;
 @ConnectorEndpoint(scheme = "test", syntax = "{type}:{transport}://{host}:{port}")
 public class TestConnector extends AbstractConnector {
 
+	@Override
 	protected void onInit() {
 		this.consumer = Optional.of(new TestConsumer());
 		this.producer = Optional.of(new TestProducer());
+	}
+	
+	@Override
+	protected void onStart() {
+		if (consumer.isPresent())
+			consumer.get().start();
+		if (producer.isPresent())
+			producer.get().start();
+	}
+
+	@Override
+	protected void onStop() {
+		if (consumer.isPresent())
+			consumer.get().stop();
+		if (producer.isPresent())
+			producer.get().stop();
 	}
 }
