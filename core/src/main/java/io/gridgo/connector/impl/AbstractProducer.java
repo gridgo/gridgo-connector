13325394ff752d4cb3dd1f820ac855676ddcebc9
anbui <anbui@ANBUI-IN.mshome.net>
2018-11-10 11:09:46 +0800

    add produceOn

--- core/src/main/java/io/gridgo/connector/impl/AbstractProducer.java
+++ core/src/main/java/io/gridgo/connector/impl/AbstractProducer.java
@@ -2,12 +2,16 @@ package io.gridgo.connector.impl;
 
 import org.joo.promise4j.Deferred;
 
+import io.gridgo.bean.BElement;
+import io.gridgo.bean.BObject;
 import io.gridgo.connector.Producer;
 import io.gridgo.framework.AbstractComponentLifecycle;
 import io.gridgo.framework.execution.ExecutionStrategy;
 import io.gridgo.framework.execution.impl.DefaultExecutionStrategy;
 import io.gridgo.framework.support.Message;
+import io.gridgo.framework.support.Payload;
 import io.gridgo.framework.support.generators.IdGenerator;
+import io.gridgo.framework.support.impl.DefaultPayload;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NonNull;
@@ -16,6 +20,10 @@ import lombok.Setter;
 public abstract class AbstractProducer extends AbstractComponentLifecycle implements Producer {
 
 	private static final ExecutionStrategy DEFAULT_CALLBACK_EXECUTOR = new DefaultExecutionStrategy();
+
+	@Getter(AccessLevel.PROTECTED)
+	@Setter(AccessLevel.PROTECTED)
+	private ExecutionStrategy producerExecutionStrategy;
 
 	@Getter(AccessLevel.PROTECTED)
 	private ExecutionStrategy callbackInvokeExecutor = DEFAULT_CALLBACK_EXECUTOR;
@@ -28,6 +36,25 @@ public abstract class AbstractProducer extends AbstractComponentLifecycle implem
 	public Producer invokeCallbackOn(final @NonNull ExecutionStrategy strategy) {
 		this.callbackInvokeExecutor = strategy;
 		return this;
+	}
+
+	@Override
+	public Producer produceOn(final @NonNull ExecutionStrategy strategy) {
+		this.producerExecutionStrategy = strategy;
+		return this;
+	}
+	
+	protected Message createMessage(BObject headers, BElement body) {
+		if (idGenerator == null)
+			return Message.newDefault(Payload.newDefault(headers, body));
+		return Message.newDefault(new DefaultPayload(idGenerator.generateId(), headers, body));
+	}
+
+	protected void ack(Deferred<Message, Exception> deferred, Message response, Exception exception) {
+		if (exception != null)
+			ack(deferred, exception);
+		else
+			ack(deferred, response);
 	}
 
 	protected void ack(Deferred<Message, Exception> deferred, Exception exception) {
