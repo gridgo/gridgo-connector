8c546d765b8fc64c635f53d2a2476e9226a26ec2
Dzung Bui <dungba.sg@gmail.com>
2018-11-12 21:51:52 +0800

    mongodb

--- connectors/gridgo-mongodb/src/main/java/io/gridgo/connector/mongodb/MongoDBConnector.java
+++ connectors/gridgo-mongodb/src/main/java/io/gridgo/connector/mongodb/MongoDBConnector.java
@@ -1,7 +1,15 @@
 package io.gridgo.connector.mongodb;
 
-import io.gridgo.connector.impl.AbstractConnector;
+import com.mongodb.async.client.MongoClient;
 
+import io.gridgo.connector.impl.AbstractConnector;
+import io.gridgo.connector.support.annotations.ConnectorEndpoint;
+
+@ConnectorEndpoint(scheme = "mongodb", syntax = "{connectionBean}")
 public class MongoDBConnector extends AbstractConnector {
 
+	protected void onInit() {
+		var connectionBean = getConnectorConfig().getPlaceholders().get("connectionBean").toString();
+		var connection = getContext().getRegistry().lookup(connectionBean, MongoClient.class);
+	}
 }
