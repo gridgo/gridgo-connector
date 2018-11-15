96626c8b443b808a3f6ca329dbf88e75fb8c372f
Bach Hoang Nguyen <hoangbach.bk@gmail.com>
2018-11-16 01:43:31 +0800

    Remove the improvement in prev commit

--- connectors/gridgo-netty4/src/main/java/io/gridgo/connector/netty4/impl/DefaultNetty4Receiver.java
+++ connectors/gridgo-netty4/src/main/java/io/gridgo/connector/netty4/impl/DefaultNetty4Receiver.java
@@ -61,14 +61,6 @@ public class DefaultNetty4Receiver extends AbstractReceiver implements FailureHa
 	protected void onStop() {
 		this.socketClient.setChannelOpenCallback(null);
 		this.socketClient.setReceiveCallback(null);
-
-		if (!this.socketClient.isRunning()) {
-			System.out.println(this.socketClient.getClass().getSimpleName()
-					+ " --> complete unlink from receiver to socket client because socket is not running");
-			this.socketClient.setChannelCloseCallback(null);
-			this.socketClient.setFailureHandler(null);
-			this.socketClient = null;
-		}
 	}
 
 	protected Deferred<Message, Exception> createDeferred() {
