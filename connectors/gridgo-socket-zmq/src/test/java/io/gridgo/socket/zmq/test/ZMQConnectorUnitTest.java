091b5bbc1c62705ff1a43f04118e3f2851eaaa56
Bach Hoang Nguyen <hoangbach.bk@gmail.com>
2018-11-20 12:13:12 +0800

    re-enable unit test

--- connectors/gridgo-socket-zmq/src/test/java/io/gridgo/socket/zmq/test/ZMQConnectorUnitTest.java
+++ connectors/gridgo-socket-zmq/src/test/java/io/gridgo/socket/zmq/test/ZMQConnectorUnitTest.java
@@ -38,7 +38,6 @@ public class ZMQConnectorUnitTest {
 	}
 
 	@Test
-//	@Ignore
 	public void testPubSubPGM() throws Exception {
 		System.out.println("Test pub/sub via PGM");
 		testPubSub("pgm", "224.2.3.4:5555");
