f61175ea2a7c8bff8e7e005dd084790414be1dbf
Bach Hoang Nguyen <hoangbach.bk@gmail.com>
2018-11-25 04:18:21 +0800

    update jettyconnector's syntax

--- connectors/gridgo-jetty-http/src/main/java/io/gridgo/connector/jetty/JettyConnector.java
+++ connectors/gridgo-jetty-http/src/main/java/io/gridgo/connector/jetty/JettyConnector.java
@@ -3,7 +3,7 @@ package io.gridgo.connector.jetty;
 import io.gridgo.connector.impl.AbstractConnector;
 import io.gridgo.connector.support.annotations.ConnectorEndpoint;
 
-@ConnectorEndpoint(scheme = "jetty", syntax = "{type}://{host}[:{port}]")
+@ConnectorEndpoint(scheme = "jetty", syntax = "{type}://{host}[:{port}][/{path}]")
 public class JettyConnector extends AbstractConnector {
 
 	@Override
