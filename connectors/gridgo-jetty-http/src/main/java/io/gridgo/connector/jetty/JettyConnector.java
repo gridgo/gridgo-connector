7bc43197342af1f714b80b227a66b1135448627a
Bach Hoang Nguyen <hoangbach.bk@gmail.com>
2018-11-25 12:35:16 +0800

    [not tested] implemented jetty connector, without support http2

--- connectors/gridgo-jetty-http/src/main/java/io/gridgo/connector/jetty/JettyConnector.java
+++ connectors/gridgo-jetty-http/src/main/java/io/gridgo/connector/jetty/JettyConnector.java
@@ -1,13 +1,31 @@
 package io.gridgo.connector.jetty;
 
+import java.util.Optional;
+
 import io.gridgo.connector.impl.AbstractConnector;
+import io.gridgo.connector.jetty.impl.DefaultJettyConsumer;
 import io.gridgo.connector.support.annotations.ConnectorEndpoint;
+import io.gridgo.utils.support.HostAndPort;
 
 @ConnectorEndpoint(scheme = "jetty", syntax = "{type}://{host}[:{port}][/{path}]")
 public class JettyConnector extends AbstractConnector {
 
 	@Override
 	protected void onInit() {
+		String type = getPlaceholder("type");
+		String host = getPlaceholder("host");
 
+		String portStr = getPlaceholder("port");
+		int port = portStr == null ? 80 : Integer.parseInt(portStr);
+
+		String path = getPlaceholder("path");
+		if (path == null || path.isBlank()) {
+			path = "/*";
+		}
+
+		var jettyConsumer = new DefaultJettyConsumer(getContext(), HostAndPort.newInstance(host, port), path, type);
+
+		this.consumer = Optional.of(jettyConsumer);
+		this.producer = Optional.of(jettyConsumer.getResponder());
 	}
 }
