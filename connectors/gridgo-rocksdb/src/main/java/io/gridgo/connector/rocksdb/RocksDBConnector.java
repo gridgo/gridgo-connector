b56de3577ce191a1c9e0fd8e8d7eeb75df627ff2
Anh Dung Bui <griever@lap00584s-mbp>
2018-12-10 16:19:23 +0700

    add dummy project for rocksdb

---
+++ connectors/gridgo-rocksdb/src/main/java/io/gridgo/connector/rocksdb/RocksDBConnector.java
@@ -0,0 +1,9 @@
+package io.gridgo.connector.rocksdb;
+
+import io.gridgo.connector.impl.AbstractConnector;
+import io.gridgo.connector.support.annotations.ConnectorEndpoint;
+
+@ConnectorEndpoint(scheme = "rocksdb", syntax = "")
+public class RocksDBConnector extends AbstractConnector {
+
+}
