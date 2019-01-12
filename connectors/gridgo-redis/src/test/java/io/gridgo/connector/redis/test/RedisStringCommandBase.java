f36f596b771d5f91d1196171b95d915f6293090a
nguyenduong94 <nguyenduong@beeketing.net>
2019-01-12 15:46:39 +0700

    setbit command unit test

--- connectors/gridgo-redis/src/test/java/io/gridgo/connector/redis/test/RedisStringCommandBase.java
+++ connectors/gridgo-redis/src/test/java/io/gridgo/connector/redis/test/RedisStringCommandBase.java
@@ -18,171 +18,202 @@ import io.gridgo.redis.command.RedisCommands;
 
 public abstract class RedisStringCommandBase {
 
-	private static final String CMD = "cmd";
+    private static final String CMD = "cmd";
 
-	private BObject buildCommand(String command) {
-		return BObject.of(CMD, command);
-	}
+    private BObject buildCommand(String command) {
+        return BObject.of(CMD, command);
+    }
 
-	protected abstract String getEndpoint();
+    protected abstract String getEndpoint();
 
-	/*
-	 * Test `bitcount` command https://redis.io/commands/bitcount
-	 */
-	public void testBitcountCommand() throws InterruptedException {
-		var connector = new DefaultConnectorFactory().createConnector(this.getEndpoint());
-		var producer = connector.getProducer().orElseThrow();
-		connector.start();
+    /*
+     * Test `bitcount` command https://redis.io/commands/bitcount
+     */
+    public void testBitcountCommand() throws InterruptedException {
+        var connector = new DefaultConnectorFactory().createConnector(this.getEndpoint());
+        var producer = connector.getProducer().orElseThrow();
+        connector.start();
 
-		var exRef = new AtomicReference<Exception>();
-		var latch = new CountDownLatch(1);
+        var exRef = new AtomicReference<Exception>();
+        var latch = new CountDownLatch(1);
 
-		producer.call(Message.ofAny(buildCommand(RedisCommands.SET), BArray.ofSequence("mykey", "foobar")))
-				.pipeDone(result -> producer.call(
-						Message.ofAny(buildCommand(RedisCommands.BITCOUNT), BArray.ofSequence("mykey", "0", "0"))))//
-				.pipeDone(result -> Common.checkLongResult(result, 4))//
-				.pipeDone(result -> producer
-						.call(Message.ofAny(buildCommand(RedisCommands.BITCOUNT), BArray.ofSequence("mykey", 1, 1))))//
-				.pipeDone(result -> Common.checkLongResult(result, 6)) //
-				.done(msg -> latch.countDown()) //
-				.fail(ex -> {
-					exRef.set(ex);
-					latch.countDown();
-				});
+        producer.call(Message.ofAny(buildCommand(RedisCommands.SET), BArray.ofSequence("mykey", "foobar")))
+                .pipeDone(result -> producer.call(
+                        Message.ofAny(buildCommand(RedisCommands.BITCOUNT), BArray.ofSequence("mykey", "0", "0"))))//
+                .pipeDone(result -> Common.checkLongResult(result, 4))//
+                .pipeDone(result -> producer
+                        .call(Message.ofAny(buildCommand(RedisCommands.BITCOUNT), BArray.ofSequence("mykey", 1, 1))))//
+                .pipeDone(result -> Common.checkLongResult(result, 6)) //
+                .done(msg -> latch.countDown()) //
+                .fail(ex -> {
+                    exRef.set(ex);
+                    latch.countDown();
+                });
 
-		latch.await();
-		connector.stop();
-		Assert.assertNull(exRef.get());
-	}
+        latch.await();
+        connector.stop();
+        Assert.assertNull(exRef.get());
+    }
 
-	/*
-	 * https://redis.io/commands/bitop
-	 */
+    /*
+     * https://redis.io/commands/bitop
+     */
 
-	public void testBitopCommand() throws InterruptedException {
-		var connector = new DefaultConnectorFactory().createConnector(this.getEndpoint());
-		var producer = connector.getProducer().orElseThrow();
-		connector.start();
+    public void testBitopCommand() throws InterruptedException {
+        var connector = new DefaultConnectorFactory().createConnector(this.getEndpoint());
+        var producer = connector.getProducer().orElseThrow();
+        connector.start();
 
-		var exRef = new AtomicReference<Exception>();
-		var latch = new CountDownLatch(1);
-		producer.call(Message.ofAny(buildCommand(RedisCommands.SET), BArray.ofSequence("key1", "foobar"))) //
-				.pipeDone(result -> producer
-						.call(Message.ofAny(buildCommand(RedisCommands.SET), BArray.ofSequence("key2", "abcdef"))))//
+        var exRef = new AtomicReference<Exception>();
+        var latch = new CountDownLatch(1);
+        producer.call(Message.ofAny(buildCommand(RedisCommands.SET), BArray.ofSequence("key1", "foobar"))) //
+                .pipeDone(result -> producer
+                        .call(Message.ofAny(buildCommand(RedisCommands.SET), BArray.ofSequence("key2", "abcdef"))))//
 
-				.pipeDone(result -> producer.call(Message.ofAny(buildCommand(RedisCommands.BITOP),
-						BArray.ofSequence(Const.BITOPAND, "resultAnd", BArray.ofSequence("key1", "key2")))))// case AND
-				.pipeDone(result -> Common.checkLongResult(result, 6))//
-				.pipeDone(result -> producer.call(Message.ofAny(buildCommand(RedisCommands.GET), "resultAnd"))) //
-				.pipeDone(result -> Common.checkStringResult(result, "`bc`ab"))
+                .pipeDone(result -> producer.call(Message.ofAny(buildCommand(RedisCommands.BITOP),
+                        BArray.ofSequence(Const.BITOPAND, "resultAnd", BArray.ofSequence("key1", "key2")))))// case AND
+                .pipeDone(result -> Common.checkLongResult(result, 6))//
+                .pipeDone(result -> producer.call(Message.ofAny(buildCommand(RedisCommands.GET), "resultAnd"))) //
+                .pipeDone(result -> Common.checkStringResult(result, "`bc`ab"))
 
-				.pipeDone(result -> producer
-						.call(Message.ofAny(buildCommand(RedisCommands.SET), BArray.ofSequence("key1", "foobar"))))//
-				.pipeDone(result -> producer
-						.call(Message.ofAny(buildCommand(RedisCommands.SET), BArray.ofSequence("key2", "abcdef"))))//
+                .pipeDone(result -> producer
+                        .call(Message.ofAny(buildCommand(RedisCommands.SET), BArray.ofSequence("key1", "foobar"))))//
+                .pipeDone(result -> producer
+                        .call(Message.ofAny(buildCommand(RedisCommands.SET), BArray.ofSequence("key2", "abcdef"))))//
 
-				.pipeDone(result -> producer.call(Message.ofAny(buildCommand(RedisCommands.BITOP),
-						BArray.ofSequence(Const.BITOPOR, "resultOr", BArray.ofSequence("key1", "key2")))))// case OR
-				.pipeDone(result -> producer.call(Message.ofAny(buildCommand(RedisCommands.GET), "resultOr")))//
-				.pipeDone(result -> Common.checkStringResult(result, "goofev"))
+                .pipeDone(result -> producer.call(Message.ofAny(buildCommand(RedisCommands.BITOP),
+                        BArray.ofSequence(Const.BITOPOR, "resultOr", BArray.ofSequence("key1", "key2")))))// case OR
+                .pipeDone(result -> producer.call(Message.ofAny(buildCommand(RedisCommands.GET), "resultOr")))//
+                .pipeDone(result -> Common.checkStringResult(result, "goofev"))
 
-				.pipeDone(result -> producer
-						.call(Message.ofAny(buildCommand(RedisCommands.SET), BArray.ofSequence("key1", "foobar"))))//
-				.pipeDone(result -> producer
-						.call(Message.ofAny(buildCommand(RedisCommands.SET), BArray.ofSequence("key2", "abcdef"))))//
+                .pipeDone(result -> producer
+                        .call(Message.ofAny(buildCommand(RedisCommands.SET), BArray.ofSequence("key1", "foobar"))))//
+                .pipeDone(result -> producer
+                        .call(Message.ofAny(buildCommand(RedisCommands.SET), BArray.ofSequence("key2", "abcdef"))))//
 
-				.pipeDone(result -> producer.call(Message.ofAny(buildCommand(RedisCommands.BITOP),
-						BArray.ofSequence(Const.BITOPXOR, "resultXor", BArray.ofSequence("key1", "key2")))))// case XOR
-				.pipeDone(result -> producer.call(Message.ofAny(buildCommand(RedisCommands.GET), "resultXor")))//
-				.pipeDone(result -> Common.checkStringResult(result, "\\a\\r\\x0c\\x06\\x04\\x14"))
+                .pipeDone(result -> producer.call(Message.ofAny(buildCommand(RedisCommands.BITOP),
+                        BArray.ofSequence(Const.BITOPXOR, "resultXor", BArray.ofSequence("key1", "key2")))))// case XOR
+                .pipeDone(result -> producer.call(Message.ofAny(buildCommand(RedisCommands.GET), "resultXor")))//
+                .pipeDone(result -> Common.checkStringResult(result, "\\a\\r\\x0c\\x06\\x04\\x14"))
 
-				.pipeDone(result -> producer
-						.call(Message.ofAny(BObject.of(CMD, "set"), BArray.ofSequence("key1", "foobar"))))//
-				.pipeDone(result -> producer
-						.call(Message.ofAny(BObject.of(CMD, "set"), BArray.ofSequence("key2", "abcdef"))))//
+                .pipeDone(result -> producer
+                        .call(Message.ofAny(BObject.of(CMD, "set"), BArray.ofSequence("key1", "foobar"))))//
+                .pipeDone(result -> producer
+                        .call(Message.ofAny(BObject.of(CMD, "set"), BArray.ofSequence("key2", "abcdef"))))//
 
-				.pipeDone(result -> producer.call(Message.ofAny(buildCommand(RedisCommands.BITOP),
-						BArray.ofSequence(Const.BITOPNOT, "dest", "key1"))))// case NOT
-				.pipeDone(result -> producer.call(Message.ofAny(buildCommand(RedisCommands.GET), "dest")))//
-				.pipeDone(result -> Common.checkStringResult(result, "\\x99\\x90\\x90\\x9d\\x9e\\x8d"))
+                .pipeDone(result -> producer.call(Message.ofAny(buildCommand(RedisCommands.BITOP),
+                        BArray.ofSequence(Const.BITOPNOT, "dest", "key1"))))// case NOT
+                .pipeDone(result -> producer.call(Message.ofAny(buildCommand(RedisCommands.GET), "dest")))//
+                .pipeDone(result -> Common.checkStringResult(result, "\\x99\\x90\\x90\\x9d\\x9e\\x8d"))
 
-				.done(result -> latch.countDown())//
-				.fail(ex -> {
-					exRef.set(ex);
-					latch.countDown();
-				});
+                .done(result -> latch.countDown())//
+                .fail(ex -> {
+                    exRef.set(ex);
+                    latch.countDown();
+                });
 
-		latch.await();
-		connector.stop();
-		Assert.assertNull(exRef.get());
+        latch.await();
+        connector.stop();
+        Assert.assertNull(exRef.get());
 
-	}
+    }
 
-	/*
-	 * https://redis.io/commands/bitpos
-	 */
-	public void testBitposCommand() throws InterruptedException {
-		var connector = new DefaultConnectorFactory().createConnector(this.getEndpoint());
-		var producer = connector.getProducer().orElseThrow();
-		connector.start();
+    /*
+     * https://redis.io/commands/bitpos
+     */
+    public void testBitposCommand() throws InterruptedException {
+        var connector = new DefaultConnectorFactory().createConnector(this.getEndpoint());
+        var producer = connector.getProducer().orElseThrow();
+        connector.start();
 
-		var exRef = new AtomicReference<Exception>();
-		var latch = new CountDownLatch(1);
+        var exRef = new AtomicReference<Exception>();
+        var latch = new CountDownLatch(1);
 
-		producer.call(Message.ofAny(buildCommand(RedisCommands.SET), BArray.ofSequence("mykey", "foo"))) //
-				.pipeDone(result -> producer
-						.call(Message.ofAny(buildCommand(RedisCommands.BITPOS), BArray.ofSequence("mykey", false))))//
-				.pipeDone(result -> Common.checkLongResult(result, 0))
-				.pipeDone(result -> producer
-						.call(Message.ofAny(buildCommand(RedisCommands.BITPOS), BArray.ofSequence("mykey", true))))//
-				.pipeDone(result -> Common.checkLongResult(result, 1))
-				.pipeDone(result -> producer
-						.call(Message.ofAny(buildCommand(RedisCommands.BITPOS), BArray.ofSequence("mykey", true, 1))))//
-				.pipeDone(result -> Common.checkLongResult(result, 9))
-				.pipeDone(result -> producer
-						.call(Message.ofAny(buildCommand(RedisCommands.BITPOS), BArray.ofSequence("mykey", false, 1))))//
-				.pipeDone(result -> Common.checkLongResult(result, 8))
-				.pipeDone(result -> producer.call(
-						Message.ofAny(buildCommand(RedisCommands.BITPOS), BArray.ofSequence("mykey", true, 1, 2))))//
-				.pipeDone(result -> Common.checkLongResult(result, 9))//
-				.done(result -> latch.countDown()).fail(ex -> {
-					exRef.set(ex);
-					latch.countDown();
-				});
+        producer.call(Message.ofAny(buildCommand(RedisCommands.SET), BArray.ofSequence("mykey", "foo"))) //
+                .pipeDone(result -> producer
+                        .call(Message.ofAny(buildCommand(RedisCommands.BITPOS), BArray.ofSequence("mykey", false))))//
+                .pipeDone(result -> Common.checkLongResult(result, 0))
+                .pipeDone(result -> producer
+                        .call(Message.ofAny(buildCommand(RedisCommands.BITPOS), BArray.ofSequence("mykey", true))))//
+                .pipeDone(result -> Common.checkLongResult(result, 1))
+                .pipeDone(result -> producer
+                        .call(Message.ofAny(buildCommand(RedisCommands.BITPOS), BArray.ofSequence("mykey", true, 1))))//
+                .pipeDone(result -> Common.checkLongResult(result, 9))
+                .pipeDone(result -> producer
+                        .call(Message.ofAny(buildCommand(RedisCommands.BITPOS), BArray.ofSequence("mykey", false, 1))))//
+                .pipeDone(result -> Common.checkLongResult(result, 8))
+                .pipeDone(result -> producer.call(
+                        Message.ofAny(buildCommand(RedisCommands.BITPOS), BArray.ofSequence("mykey", true, 1, 2))))//
+                .pipeDone(result -> Common.checkLongResult(result, 9))//
+                .done(result -> latch.countDown()).fail(ex -> {
+            exRef.set(ex);
+            latch.countDown();
+        });
 
-		latch.await();
-		connector.stop();
-		Assert.assertNull(exRef.get());
-	}
+        latch.await();
+        connector.stop();
+        Assert.assertNull(exRef.get());
+    }
 
-	/*
-	 * https://redis.io/commands/bitfield
-	 */
+    /*
+     * https://redis.io/commands/bitfield
+     */
 
-	public void testBitFieldCommand() throws InterruptedException {
-		var connector = new DefaultConnectorFactory().createConnector(this.getEndpoint());
-		var producer = connector.getProducer().orElseThrow();
-		connector.start();
+    public void testBitFieldCommand() throws InterruptedException {
+        var connector = new DefaultConnectorFactory().createConnector(this.getEndpoint());
+        var producer = connector.getProducer().orElseThrow();
+        connector.start();
 
-		var exRef = new AtomicReference<Exception>();
-		var latch = new CountDownLatch(1);
-		producer.call(Message.ofAny(buildCommand(RedisCommands.BITFIELD),
-				BArray.ofSequence("mykey", "INCRBY", "i5", "100", "1", "GET", "u4", "0")))//
-				.pipeDone(result -> {
-					var responses = result.body().asArray();
-					if (1L == responses.get(0).asValue().getLong() && 0L == responses.get(1).asValue().getLong()) {
-						return Promise.of(result);
-					}
-					return Promise.ofCause(new RuntimeException());
-				})//
-				.done(result -> latch.countDown()).fail(ex -> {
-					exRef.set(ex);
-					latch.countDown();
-				});
+        var exRef = new AtomicReference<Exception>();
+        var latch = new CountDownLatch(1);
 
-		latch.await();
-		connector.stop();
-		Assert.assertNull(exRef.get());
-	}
+        producer.call(Message.ofAny(buildCommand(RedisCommands.SET), BArray.ofSequence("mykey", "100")))//
+                .pipeDone(result -> producer.call(Message.ofAny(buildCommand(RedisCommands.BITFIELD),
+                        BArray.ofSequence("mykey", "INCRBY", "i5", "100", "1", "GET", "u4", "0"))))//
+                .pipeDone(result -> {
+                    var responses = result.body().asArray();
+                    if (1L == responses.get(0).asValue().getLong() && 0L == responses.get(1).asValue().getLong()) {
+                        return Promise.of(result);
+                    }
+                    return Promise.ofCause(new RuntimeException());
+                })//
+                .done(result -> latch.countDown())//
+                .fail(ex -> {
+                    exRef.set(ex);
+                    latch.countDown();
+                });
+
+        latch.await();
+        connector.stop();
+        Assert.assertNull(exRef.get());
+    }
+    /*
+     * https://redis.io/commands/setbit
+     */
+
+    public void testSetBitCommand() throws InterruptedException {
+        var connector = new DefaultConnectorFactory().createConnector(this.getEndpoint());
+        var producer = connector.getProducer().orElseThrow();
+        connector.start();
+
+        var exRef = new AtomicReference<Exception>();
+        var latch = new CountDownLatch(1);
+        producer.call(Message.ofAny(buildCommand(RedisCommands.DEL), "mykey"))//
+                .pipeDone(result -> producer.call(Message.ofAny(buildCommand(RedisCommands.SETBIT), BArray.ofSequence("mykey", 7, 1))))//
+                .pipeDone(result -> Common.checkLongResult(result, 0))//
+                .pipeDone(result -> producer.call(Message.ofAny(buildCommand(RedisCommands.SETBIT), BArray.ofSequence("mykey", 7, 0))))//
+                .pipeDone(result -> Common.checkLongResult(result, 1))//
+                .pipeDone(result -> producer.call(Message.ofAny(buildCommand(RedisCommands.GET), "mykey")))//
+                .pipeDone(result -> Common.checkStringResult(result, "\u0000"))//
+                .done(result -> latch.countDown())
+                .fail(ex -> {
+                    exRef.set(ex);
+                    latch.countDown();
+                });
+        
+        latch.await();
+        connector.stop();
+        Assert.assertNull(exRef.get());
+    }
 
 }
--- connectors/gridgo-redis/src/test/java/io/gridgo/connector/redis/test/SingleRedisStringCommand.java
+++ connectors/gridgo-redis/src/test/java/io/gridgo/connector/redis/test/SingleRedisStringCommand.java
@@ -7,31 +7,37 @@ import lombok.Getter;
 
 public class SingleRedisStringCommand extends RedisStringCommandBase {
 
-	@Getter(AccessLevel.PROTECTED)
-	private final String endpoint = "redis:single://[localhost:6379]";
+    @Getter(AccessLevel.PROTECTED)
+    private final String endpoint = "redis:single://[localhost:6379]";
 
-	@Test
-	@Override
-	public void testBitcountCommand() throws InterruptedException {
-		super.testBitcountCommand();
-	}
+    @Test
+    @Override
+    public void testBitcountCommand() throws InterruptedException {
+        super.testBitcountCommand();
+    }
 
-	@Test
-	@Override
-	public void testBitopCommand() throws InterruptedException {
-		super.testBitopCommand();
-	}
+    @Test
+    @Override
+    public void testBitopCommand() throws InterruptedException {
+        super.testBitopCommand();
+    }
 
-	@Test
-	@Override
-	public void testBitposCommand() throws InterruptedException {
-		super.testBitposCommand();
-	}
-	
-	@Test
-	@Override
-	public void testBitFieldCommand() throws InterruptedException {
-		super.testBitFieldCommand();
-	}
+    @Test
+    @Override
+    public void testBitposCommand() throws InterruptedException {
+        super.testBitposCommand();
+    }
+
+    @Test
+    @Override
+    public void testBitFieldCommand() throws InterruptedException {
+        super.testBitFieldCommand();
+    }
+
+    @Test
+    @Override
+    public void testSetBitCommand() throws InterruptedException {
+        super.testSetBitCommand();
+    }
 
 }
