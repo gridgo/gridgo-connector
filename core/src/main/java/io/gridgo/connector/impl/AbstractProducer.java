236db4982e5eb28069ec404c17c95d6d36f11b70
Anh Dung Bui <dungba.sg@gmail.com>
2019-01-15 15:03:10 +0700

    refactor

--- core/src/main/java/io/gridgo/connector/impl/AbstractProducer.java
+++ core/src/main/java/io/gridgo/connector/impl/AbstractProducer.java
@@ -1,7 +1,5 @@
 package io.gridgo.connector.impl;
 
-import org.joo.promise4j.Deferred;
-
 import io.gridgo.bean.BElement;
 import io.gridgo.bean.BObject;
 import io.gridgo.connector.Producer;
@@ -13,32 +11,13 @@ import io.gridgo.framework.support.impl.DefaultPayload;
 import lombok.Getter;
 import lombok.NonNull;
 
-public abstract class AbstractProducer extends AbstractComponentLifecycle implements Producer {
+public abstract class AbstractProducer extends AbstractComponentLifecycle implements Producer, ProducerAck {
 
     @Getter
     private final ConnectorContext context;
 
-    private ProducerAck ack;
-
     protected AbstractProducer(final @NonNull ConnectorContext context) {
         this.context = context;
-        this.ack = new DefaultProducerAck(context);
-    }
-
-    protected void ack(Deferred<Message, Exception> deferred) {
-        ack.ack(deferred);
-    }
-
-    protected void ack(Deferred<Message, Exception> deferred, Exception exception) {
-        ack.ack(deferred, exception);
-    }
-
-    protected void ack(Deferred<Message, Exception> deferred, Message response) {
-        ack.ack(deferred, response);
-    }
-
-    protected void ack(Deferred<Message, Exception> deferred, Message response, Exception exception) {
-        ack(deferred, response, exception);
     }
 
     protected Message createMessage(BObject headers, BElement body) {
