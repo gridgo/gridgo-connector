83dc3cc1cdb0e704db15849377bf21995e64baf3
Anh Dung Bui <dungba.sg@gmail.com>
2019-01-15 14:58:37 +0700

    split ack() into new class

--- core/src/main/java/io/gridgo/connector/impl/AbstractProducer.java
+++ core/src/main/java/io/gridgo/connector/impl/AbstractProducer.java
@@ -5,6 +5,7 @@ import org.joo.promise4j.Deferred;
 import io.gridgo.bean.BElement;
 import io.gridgo.bean.BObject;
 import io.gridgo.connector.Producer;
+import io.gridgo.connector.ProducerAck;
 import io.gridgo.connector.support.config.ConnectorContext;
 import io.gridgo.framework.impl.AbstractComponentLifecycle;
 import io.gridgo.framework.support.Message;
@@ -17,53 +18,30 @@ public abstract class AbstractProducer extends AbstractComponentLifecycle implem
     @Getter
     private final ConnectorContext context;
 
+    private ProducerAck ack;
+
     protected AbstractProducer(final @NonNull ConnectorContext context) {
         this.context = context;
+        this.ack = new DefaultProducerAck(context);
     }
 
     protected void ack(Deferred<Message, Exception> deferred) {
-        if (deferred != null) {
-            context.getCallbackInvokerStrategy().execute(() -> tryResolve(deferred, null));
-        }
+        ack.ack(deferred);
     }
 
     protected void ack(Deferred<Message, Exception> deferred, Exception exception) {
-        if (exception != null)
-            getLogger().error("Exception caught while acknowledging response", exception);
-        if (deferred != null) {
-            context.getCallbackInvokerStrategy().execute(() -> {
-                if (exception == null) {
-                    tryResolve(deferred, null);
-                } else {
-                    deferred.reject(exception);
-                }
-            });
-        }
+        ack.ack(deferred, exception);
     }
 
     protected void ack(Deferred<Message, Exception> deferred, Message response) {
-        if (deferred != null) {
-            context.getCallbackInvokerStrategy().execute(() -> tryResolve(deferred, response));
-        }
+        ack.ack(deferred, response);
     }
 
     protected void ack(Deferred<Message, Exception> deferred, Message response, Exception exception) {
-        if (exception != null)
-            ack(deferred, exception);
-        else
-            ack(deferred, response);
+        ack(deferred, response, exception);
     }
 
     protected Message createMessage(BObject headers, BElement body) {
         return Message.of(new DefaultPayload(context.getIdGenerator().generateId(), headers, body));
-    }
-
-    private Deferred<Message, Exception> tryResolve(Deferred<Message, Exception> deferred, Message response) {
-        try {
-            return deferred.resolve(response);
-        } catch (Exception ex) {
-            getLogger().error("Exception caught while trying to resolve deferred", ex);
-            return deferred.reject(ex);
-        }
     }
 }
