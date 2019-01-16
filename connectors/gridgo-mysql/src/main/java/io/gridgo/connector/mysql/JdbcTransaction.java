bae4581b3bd014835a37c0116cb6b64e7112426f
manhcuongbk56 <cuong.le2@tiki.vn>
2019-01-16 16:03:17 +0700

    Close connection after commit or rollback

--- connectors/gridgo-mysql/src/main/java/io/gridgo/connector/mysql/JdbcTransaction.java
+++ connectors/gridgo-mysql/src/main/java/io/gridgo/connector/mysql/JdbcTransaction.java
@@ -2,19 +2,16 @@ package io.gridgo.connector.mysql;
 
 
 import io.gridgo.connector.impl.AbstractProducer;
-import io.gridgo.connector.mysql.support.Helper;
 import io.gridgo.connector.mysql.support.JdbcOperationException;
 import io.gridgo.connector.support.config.ConnectorContext;
-import io.gridgo.connector.support.transaction.AbstractTransaction;
 import io.gridgo.framework.support.Message;
 import lombok.extern.slf4j.Slf4j;
 import org.jdbi.v3.core.Handle;
+import org.jdbi.v3.core.transaction.TransactionException;
 import org.joo.promise4j.Promise;
 import org.joo.promise4j.impl.CompletableDeferredObject;
 import org.joo.promise4j.impl.SimpleFailurePromise;
 
-import java.util.HashMap;
-import java.util.Map;
 
 import static io.gridgo.connector.mysql.JdbcConstants.*;
 
@@ -54,10 +51,11 @@ class JdbcTransaction extends JdbcClient {
     @Override
     protected Promise<Message, Exception> doCommit() {
         var deferred = new CompletableDeferredObject<Message, Exception>();
-        try (var tempHandle = handle) {
-            tempHandle.commit();
+        try (var temHandle = handle) {
+            temHandle.commit();
             ack(deferred, Message.ofEmpty());
-        } catch (Exception ex) {
+        } catch (TransactionException ex) {
+            ex.printStackTrace();
             ack(deferred, ex);
         }
         return deferred;
@@ -66,9 +64,12 @@ class JdbcTransaction extends JdbcClient {
     @Override
     protected Promise<Message, Exception> doRollback() {
         var deferred = new CompletableDeferredObject<Message, Exception>();
-        try (var tempHandle = handle) {
-            tempHandle.rollback();
-            Helper.ack(deferred, null, null);
+        try (var temHandle = handle) {
+            temHandle.rollback();
+            ack(deferred, Message.ofEmpty());
+        }catch (Exception ex){
+            ex.printStackTrace();
+            ack(deferred, ex);
         }
         return deferred;
     }
