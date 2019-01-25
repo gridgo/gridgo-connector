510795d114b5dd475a3edd0ecfe9d88a9a06ed5d
Anh Dung Bui <dungba.sg@gmail.com>
2019-01-25 11:09:07 +0700

    rename constants

--- connectors/gridgo-jdbc/src/main/java/io/gridgo/connector/jdbc/JdbcConstants.java
+++ connectors/gridgo-jdbc/src/main/java/io/gridgo/connector/jdbc/JdbcConstants.java
@@ -7,15 +7,15 @@ public class JdbcConstants {
 
     public static final String OPERATION = "JDBC_Operation";
 
-    public static final String OPERATION_SELECT = "JDBC_select";
+    public static final String OPERATION_SELECT = "select";
 
-    public static final String OPERATION_UPDATE = "JDBC_update";
+    public static final String OPERATION_UPDATE = "update";
 
-    public static final String OPERATION_DELETE = "JDBC_delete";
+    public static final String OPERATION_DELETE = "delete";
 
-    public static final String OPERATION_INSERT = "JDBC_insert";
+    public static final String OPERATION_INSERT = "insert";
 
-    public static final String OPERATION_EXCUTE = "JDBC_excute";
+    public static final String OPERATION_EXECUTE = "execute";
 
     static final int DEFAULT_CONNECTION_POOL_MIN_POOL_SIZE = 5;
 
