2360b601cce6e9d30fd35948b0e7462011d0d5e4
Bach Hoang Nguyen <hoangbach.bk@gmail.com>
2019-03-24 23:29:23 +0800

    [Jetty] refactor

--- connectors/gridgo-jetty-http/src/main/java/io/gridgo/connector/jetty/parser/DefaultHttpRequestParser.java
+++ connectors/gridgo-jetty-http/src/main/java/io/gridgo/connector/jetty/parser/DefaultHttpRequestParser.java
@@ -1,11 +1,10 @@
 package io.gridgo.connector.jetty.parser;
 
+import static io.gridgo.connector.httpcommon.HttpContentType.APPLICATION_OCTET_STREAM;
+import static io.gridgo.connector.httpcommon.HttpContentType.DEFAULT_TEXT;
 import static io.gridgo.connector.jetty.support.HttpEntityHelper.parseAsMultiPart;
 
 import java.io.StringWriter;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -18,31 +17,33 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class DefaultHttpRequestParser extends AbstractHttpRequestParser {
 
-    protected static final Set<String> NO_BODY_METHODS = new HashSet<>(Arrays.asList("get", "delete", "options"));
-
     private final String format;
 
     @Override
     protected BElement extractBody(HttpServletRequest request) throws Exception {
         if (!NO_BODY_METHODS.contains(request.getMethod().toLowerCase().trim())) {
-            String reqContentType = request.getContentType();
-            HttpContentType contentType = HttpContentType.forValueOrDefault(reqContentType, HttpContentType.DEFAULT_TEXT);
+
+            HttpContentType contentType = HttpContentType.forValueOrDefault(request.getContentType(), DEFAULT_TEXT);
+
+            if (contentType == APPLICATION_OCTET_STREAM) {
+                return BElement.ofBytes(request.getInputStream(), format);
+            }
+
             if (contentType.isMultipartFormat()) {
                 return parseAsMultiPart(request.getParts());
             }
+
             if (contentType.isJsonFormat()) {
                 return BElement.ofJson(request.getInputStream());
             }
+
             if (contentType.isTextFormat()) {
-                try (StringWriter out = new StringWriter()) {
-                    request.getReader().transferTo(out);
-                    return BValue.of(out.toString());
-                }
+                StringWriter out = new StringWriter();
+                request.getReader().transferTo(out);
+                return BValue.of(out.toString());
             }
+
             if (contentType.isBinaryFormat()) {
-                if (contentType == HttpContentType.APPLICATION_OCTET_STREAM) {
-                    return BElement.ofBytes(request.getInputStream(), format);
-                }
                 return BReference.of(request.getInputStream());
             }
         }
--- connectors/gridgo-jetty-http/src/main/java/io/gridgo/connector/jetty/parser/HttpRequestParser.java
+++ connectors/gridgo-jetty-http/src/main/java/io/gridgo/connector/jetty/parser/HttpRequestParser.java
@@ -1,5 +1,7 @@
 package io.gridgo.connector.jetty.parser;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Set;
 
 import javax.servlet.http.HttpServletRequest;
@@ -8,6 +10,8 @@ import io.gridgo.connector.jetty.server.JettyServletContextHandlerOption;
 import io.gridgo.framework.support.Message;
 
 public interface HttpRequestParser {
+
+    static final Set<String> NO_BODY_METHODS = new HashSet<>(Arrays.asList("get", "delete", "options"));
 
     static HttpRequestParser newDefault(String format) {
         return new DefaultHttpRequestParser(format);
