713182c359f555ea197a6d052d384a20a9b76d47
Bach Hoang Nguyen <hoangbach.bk@gmail.com>
2019-04-06 16:04:39 +0800

    added netty4echoserver for test

---
+++ connectors/gridgo-netty4/src/test/java/io/gridgo/connector/netty4/test/Netty4EchoServer.java
@@ -0,0 +1,65 @@
+package io.gridgo.connector.netty4.test;
+
+import io.gridgo.connector.Connector;
+import io.gridgo.connector.ConnectorResolver;
+import io.gridgo.connector.Consumer;
+import io.gridgo.connector.Producer;
+import io.gridgo.connector.impl.resolvers.ClasspathConnectorResolver;
+import io.gridgo.connector.support.exceptions.FailureHandlerAware;
+import io.gridgo.utils.ThreadUtils;
+
+public class Netty4EchoServer {
+
+    private final static ConnectorResolver RESOLVER = new ClasspathConnectorResolver("io.gridgo.connector");
+
+    public static void main(String[] args) {
+        String transport = "ws";
+        String host = "localhost:8889";
+        String path = "websocket";
+
+        if (args.length > 0) {
+            transport = args[0];
+            if (args.length > 1) {
+                host = args[1];
+                if (args.length > 2) {
+                    path = args[2];
+                }
+            }
+        }
+
+        String endpoint = "netty4:server:" + transport + "://" + host + (path == null ? "" : ("/" + path));
+        Connector serverConnector = RESOLVER.resolve(endpoint);
+
+        // server side
+        final Consumer serverConsumer = serverConnector.getConsumer().get();
+        final Producer serverResponder = serverConnector.getProducer().get();
+
+        serverConsumer.subscribe((msg) -> {
+            String socketMessageType = (String) msg.getMisc().get("socketMessageType");
+            switch (socketMessageType) {
+            case "open":
+                System.out.println("socket open, routing id: " + msg.getRoutingId().get());
+                break;
+            case "close":
+                System.out.println("socket closed, routing id: " + msg.getRoutingId().get());
+                break;
+            default:
+                System.out.println("got msg from source: " + msg.getMisc().get("source"));
+                serverResponder.send(msg);
+            }
+        });
+
+        ((FailureHandlerAware<?>) serverConsumer).setFailureHandler((cause) -> {
+            cause.printStackTrace();
+            return null;
+        });
+
+        serverConnector.start();
+        System.out.println("Echo server started at: " + endpoint);
+
+        ThreadUtils.registerShutdownTask(() -> {
+            serverConnector.stop();
+        });
+    }
+
+}
